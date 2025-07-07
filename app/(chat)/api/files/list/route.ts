1cbdf94eb11bfd114dbe538d5620569f108433d7
David Castro <david.castro@pslgroup.com>
2025-07-07 11:25:23 -0600

    first commit

---
+++ app/(chat)/api/files/list/route.ts
@@ -0,0 +1,26 @@
+import { auth } from "@/app/(auth)/auth";
+import { list } from "@vercel/blob";
+
+export async function GET() {
+  let session = await auth();
+
+  if (!session) {
+    return Response.redirect("/login");
+  }
+
+  const { user } = session;
+
+  if (!user) {
+    return Response.redirect("/login");
+  }
+
+  console.log("Token:", process.env.BLOB_READ_WRITE_TOKEN);
+  const { blobs } = await list({ prefix: user.email! });
+
+  return Response.json(
+    blobs.map((blob) => ({
+      ...blob,
+      pathname: blob.pathname.replace(`${user.email}/`, ""),
+    }))
+  );
+}
---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env.local",
+});
+
+export default defineConfig({
+  schema: "./schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.POSTGRES_URL!,
+    ssl: false, // Set to true if your database requires SSL
+  },
+});
